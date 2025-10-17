f826a75adb9bcb0794340f66d6bd7e1ebcc95395
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-17 14:22:11 +0200

    associa funzioni e rotte

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("prova")
+// importo il framework express
+const express = require("express");
+// creiamo un istanza di express
+const app = express();
+// impostiano un ref per il numero della porta
+const port = 3000;
+
+// importiamo modulo router i post
+const postRouter = require("./routers/posts")
+
+// usiamo il middleware static di express (per rendere disponibile i file statici)
+app.use(express.static('public'));
+
+
+// impostiamo la rotta di home
+app.get("/", (req, res) => {
+    console.log("hai richiesto la rotta di index");
+
+    res.send('<h1>Ecco la home della API del nostro blog</h1>')
+})
+
+// rotte per i post
+app.use("/posts", postRouter);
+
+
+// mettiamo in ascolto il server sulla porta definita
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,26 +1,30 @@
-//index
-function index(req, res){
-    res.send("Lista Posts")
-};
-//show
-function show(req, res){
-    res.send("dettagli Post: " + req.params.id)
-};
-//store
-function store(req, res){
-    res.send("Creazione nuovo post")
-};
-//update
-function update(req, res){
-    res.send("Modifica integrale del post: " + req.params.id)
-};
-//modify
-function modify(req, res){
-    res.send("Modifica parziale del post: " + req.params.id)
-};
-//destroy
-function destroy(req, res) {
-    res.send("Eliminazione del post: " + req.params.id)
+// importiamo i dati della risorsa
+const postIndex = require('../data/index');
+
+function index(req, res) {
+     res.send("Lista Posts")
 };
 
-module.exports = router;
+
+function show(req, res) {
+    res.send("dettagli Post: " + req.params.id)
+}
+
+function store(req, res) {
+    res.send("Creazione nuovo post")
+}
+
+function update(req, res) {
+    res.send("Modifica integrale del post: " + req.params.id)
+}
+
+function modify(req, res) {
+    res.send("Modifica parziale del post: " + req.params.id)
+}
+
+function destroy(req, res) {
+    res.send("Eliminazione del post: " + req.params.id)
+}
+
+// esportiamo tutto
+module.exports = { index, show, store, update, modify, destroy }
