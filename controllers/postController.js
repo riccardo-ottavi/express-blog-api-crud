19c5f58bd913e6c08ccd2d8fcaaa499e0f383f1a
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-17 14:38:01 +0200

    finalizza associazione

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,13 +1,31 @@
 // importiamo i dati della risorsa
-const postIndex = require('../data/index');
+const postsIndex = require('../data/index');
 
 function index(req, res) {
-     res.send("Lista Posts")
+    res.json(postsIndex);
 };
 
 
 function show(req, res) {
-    res.send("dettagli Post: " + req.params.id)
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il post tramite id
+    const post = postsIndex.find(post => post.id === id);
+
+    // Facciamo il controllo
+    if (!post) {
+
+        //Imposto lo status 404
+        res.status(404)
+
+        // Restituisco un JSON con le altre informazioni
+        return res.json({
+            error: "Not Found",
+            message: "post non trovato"
+        })
+    }
+    // Restituiamolo sotto forma di JSON   
+    res.json(post);
 }
 
 function store(req, res) {
@@ -23,8 +41,33 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
-    res.send("Eliminazione del post: " + req.params.id)
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il post tramite id
+    const post = postsIndex.find(post => post.id === id);
+
+    // Piccolo controllo
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    // Rimuoviamo il post dal menu
+    postsIndex.splice(postsIndex.indexOf(post), 1);
+
+    // aggiungiamo controllo in log
+    console.log(postsIndex);
+
+    // Restituiamo lo status corretto
+    res.sendStatus(204)
 }
+
 
 // esportiamo tutto
 module.exports = { index, show, store, update, modify, destroy }
